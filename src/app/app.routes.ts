cfae333f1bbe57a2e476f8394c14b642cd85a144
toni Filho <oficinadosdrones@gmail.com>
2024-12-27 20:58:21 +0000

    created components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' }
+  ];
