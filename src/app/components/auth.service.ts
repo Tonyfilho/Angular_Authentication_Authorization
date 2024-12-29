fb787383cefe47790f180bc2451722a416baee25
toni Filho <oficinadosdrones@gmail.com>
2024-12-29 19:06:35 +0000

    auth services

--- src/app/components/auth.service.ts
+++ src/app/components/auth.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IUser } from '../share/user';
+import { Observable } from 'rxjs';
+import { ILogin } from '../share/login';
+
+const POSTREGISTER = 'https://api.realworld.io/api/users';
+const POSTLOGIN = 'https://api.realworld.io/api/users/login';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  register = (register: IUser): Observable<{user: IUser}> => {
+    return this.http.post<{ user: IUser }>(POSTREGISTER, { user: register });
+  };
+  login = (user: ILogin): Observable<ILogin> => {
+    return this.http.post<ILogin>(POSTLOGIN, { user });
+  };
 }
---
+++ src/app/share/login.ts
@@ -0,0 +1,6 @@
+export interface ILogin {
+  user: {
+    email: string;
+    username: string;
+  };
+}
